0e5285ddd28387c8c72d05237d4f3e09bb505251
ZhiyuZhang <pmozhang@gmail.com>
2016-11-11 16:05:05 +0100

    upadte the on-source time and theoritical noise calculation

--- initialisation.sh
+++ initialisation.sh
@@ -13,6 +13,7 @@ rm jcmt_cube.sdf
 
 # output a Tsys array for each subscan and each receptor 
 hdstrace a20151202_00030_01_0001.sdf.more.acsis.tsys nlines=all > AllTsys.dat  
+hdstrace a20151202_00030_01_0001.sdf.more.JCMTSTATE.ACS_EXPOSURE nlines=all > All_on_time.dat  
 # convert the JCMT format data to .fits format 
 ndf2fits a20151202_00030_01_0001.sdf a20151202_00030_01_0001.fits allowtab comp=d encoding='fits-wcs'  
 # get the locations for the receptors for each subscan 
@@ -29,4 +30,18 @@ sed -i '' '1d' AllTsys.dat
 sed -i '' 's/E/\'$'\n''/g' AllTsys.dat
 # delete the last line
 sed -i '' '$ d' AllTsys.dat 
+
+
+
+#change return to space -- don't know why sed could not make it in mac...
+cat All_on_time.dat | tr  '\n' ' ' > All_on_time1.dat
+mv All_on_time1.dat All_on_time.dat 
+# change ) to the return character
+sed -i '' 's/)/\'$'\n''/g' All_on_time.dat
+# delete the first line
+sed -i '' '1d' All_on_time.dat
+# change E to the return character
+sed -i '' 's/E/\'$'\n''/g' All_on_time.dat
+# delete the last line
+sed -i '' '$ d' All_on_time.dat 
 
