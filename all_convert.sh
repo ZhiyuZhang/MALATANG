60425b0caf9b5c99bd223d3dc59e659895981b88
ZhiyuZhang <pmozhang@gmail.com>
2016-12-15 04:55:06 +0100

    update with the output file name

--- all_convert.sh
+++ all_convert.sh
@@ -1,7 +1,7 @@
 for filename in *.sdf; do 
-    ./initialisation.sh                     "$filename"
-    ipython convert_JCMT_to_sdfits.py       ${filename:0:23}.fits 
-    class @ convert_fits_to_class.class M83_${filename:0:23}
+    ./initialisation.sh                        "$filename"
+    ipython convert_JCMT_to_sdfits.py           ${filename:0:23}.fits 
+    class @ convert_fits_to_class.class         ${filename:0:23}
 done
 
 
