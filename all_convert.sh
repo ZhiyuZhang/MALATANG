d17ee3be1c3343b19bbcba1d40408e4f48468643
ZhiyuZhang <pmozhang@gmail.com>
2018-11-28 14:06:22 +0100

    update coordinate file

--- all_convert.sh
+++ all_convert.sh
@@ -7,7 +7,8 @@
 for filename in *.sdf; do 
     ./initialisation.sh                     "$filename"
     ipython convert_JCMT_to_sdfits.py       ${filename:0:23}.fits 
-    class @ convert_fits_to_class.class     ${filename:0:23} $1 $2 
+    line=$(head -1 coordinate.dat)
+    class @ convert_fits_to_class.class     ${filename:0:23} $line
 done
 
 
